916bae384cee8b9a7b28d0f0fac89bf26dcf69a6
divergent3 <sergejsabala@gmail.com>
2020-08-21 00:43:41 +0300

    Добавьте файлы проекта.

---
+++ Cranes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cranes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите общее число жупавликов");
+            int S = Convert.ToInt32( Console.ReadLine());
+            int Sum = S / 2;
+            int Sum1 = Sum / 2;
+            Console.WriteLine($" Катя сделала {Sum}  журавликов,  Серёжа сделал {Sum1} журавликов, Петя сделал {Sum1} журавликов.");
+            Console.ReadKey();
+        }
+    }
+}
